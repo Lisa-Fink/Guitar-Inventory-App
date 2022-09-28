61bcd15763c7a7494b5c19b39431d1416d37f455
Lisa Fink <lisadfink@gmail.com>
2022-09-28 13:33:52 -0400

    Created get function and guitar instance form

--- controllers/guitarInstanceController.js
+++ controllers/guitarInstanceController.js
@@ -1,5 +1,56 @@
-exports.guitar_create_get = (req, res, next) => {
-  res.send('incomplete');
+const Brand = require('../models/brand');
+const Guitar = require('../models/guitar');
+const GuitarInstance = require('../models/guitarInstance');
+const Series = require('../models/series');
+
+const { body, validationResult } = require('express-validator');
+
+exports.guitar_create_get = async (req, res, next) => {
+  const brandList = await Brand.find({}, { name: 1 });
+  const modelList = await Guitar.find({}, { _id: 0, model: 1, brand: 1 });
+  const seriesList = await Series.find(
+    {},
+    { _id: 0, model: 1, brand: 1, series: 1, colors: 1 }
+  );
+
+  // lookup the colors in the series
+  const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple',
+    'white',
+    'black',
+    'sunburst',
+  ];
+
+  // created copy because the original isn't passing in colors
+  copiedSeries = [];
+
+  seriesList.forEach((series) => {
+    copy = {
+      brand: series.brand,
+      series: series.series,
+      model: series.model,
+      colors: series.colors,
+    };
+    copiedSeries.push(copy);
+  });
+
+  res.render('guitar_instance_form', {
+    title: 'Add a Guitar',
+    // status true is updating not creating
+    status: false,
+    defaultModel: req.query.model,
+    defaultBrand: req.query.brand,
+    defaultSeries: req.query.series,
+    brands: brandList,
+    models: modelList,
+    series: copiedSeries,
+    colors: colors,
+  });
 };
 
 exports.guitar_create_post = (req, res, next) => {
